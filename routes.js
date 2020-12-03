50082cafdf333f72170a8bd93c4fad506baa705c
Laura Zambrano <laura.zambrano@eyss.io>
2020-12-03 18:47:58 -0400

    fotos transformadas
    Signed-off-by: Laura Zambrano <laura.zambrano@eyss.io>

--- routes.js
+++ routes.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const path = require('path');
+const { promisify } = require('util');
+const fs = require('fs');
+const convert = require('heic-convert');
 const app = express();
 
 //routes 
 app.post('/fotos', async (req, res) => {
     if(req.files){
         let photo = req.files.photo;
+
+        const outputBuffer = await convert({
+            buffer: photo.data, // the HEIC file buffer
+            format: 'JPEG',      // output format
+            quality: 1           // the jpeg compression quality, between 0 and 1
+        });
+        
+        photo.name = photo.name.split(".")[0];
+        await promisify(fs.writeFile)(path.join(__dirname, '/transformadas/') + photo.name + '.jpg', outputBuffer);
+
         console.log("foto recibida: ", photo);
 
         if(!photo.mimetype){
@@ -15,7 +28,6 @@ app.post('/fotos', async (req, res) => {
         }else {
             let ext = photo.mimetype.split('/')[1];
             ext = ext.split("+")[0];
-            photo.name = photo.name.split(".")[0];
             let urlPhoto = path.join(__dirname, '/fotos/') + photo.name + '.' + ext;
             photo.mv(urlPhoto, async function(err) {
                 if (err) {
